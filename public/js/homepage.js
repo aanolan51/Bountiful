cc52bf29ed2342347f6b3e1171059f84cff3d6e7
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-07-07 12:34:43 -0700

    added js file to remove hero image when not in homepage and edited main to remove div from the {{{body}}}

---
+++ public/js/homepage.js
@@ -0,0 +1,12 @@
+const imgClassSelector = document.querySelector("#hero-image")
+
+const heroPicture = () => {
+    console.log(document.location.pathname)
+    if(document.location.pathname === "/"){
+        imgClassSelector.setAttribute("class","image")
+    } else {
+        imgClassSelector.setAttribute("class","no-hero-image")
+    }
+}
+
+heroPicture();
