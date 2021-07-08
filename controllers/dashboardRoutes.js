f1a4d7ffe6f60f2f099cc7365e33c612bdb7d73b
helloam <oldcamp@Offgregs-MacBook-Pro.local>
2021-07-07 19:45:02 -0700

    category title fix

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Item, Catergory, User } = require('../models');
+const { Item, Category, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 //Add in withAuth to include the authorization for logins. Only go to dashboard page when logged in. Show all items for the user:
@@ -9,7 +9,7 @@ router.get('/', withAuth, async (req, res) => {
       const userItemData = await Item.findAll({
         where: {user_id: req.session.user_id},
         attributes: { exclude: ['password'] },
-        include: [{ model: User }, {model: Catergory}],
+        include: [{ model: User }, {model: Category}],
       });
   
       //Getting multiple posts, so need to map the data:
@@ -32,7 +32,7 @@ router.get('/sellerProfile/:id', withAuth, async (req, res) => {
           //Console.log this to make sure the id matches the seller id:
         where: {id: req.params.id},
         attributes: { exclude: ['password'] },
-        include: [{ model: User }, {model: Catergory}],
+        include: [{ model: User }, {model: Category}],
       });
   
       //Getting multiple posts, so need to map the data:
