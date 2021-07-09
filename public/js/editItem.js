022b87285277059824a2e3c8ebf2514357fd7399
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-07-09 09:06:04 -0700

    updated cards and added front-side js to edit items on dashboard

--- public/js/editItem.js
+++ public/js/editItem.js
@@ -1,9 +1,18 @@
+// const e = require("express");
+
+
 const editItemFunc = async (event) => {
     event.preventDefault();
-  
+
     const title = document.querySelector('#itemTitle').value.trim();
-    
-    
+    const name = document.querySelector("#item-name").value.trim();
+    const description = document.querySelector("#item-description").value.trim();
+    const unit = document.querySelector("#item-unit").value.trim();
+    const quantity = document.querySelector("#item-quantity").value.trim();
+    const price = document.querySelector("#item-price").value.trim();
+    const category = document.querySelector("#item-categories").value.trim();
+
+      
     // console.log("EDIT EDIT ------------------------------------------------------------ ")
     
   
@@ -13,12 +22,12 @@ const editItemFunc = async (event) => {
 
       const response = await fetch(`/api/items/edititem/${id}`, {
         method: 'PUT',
-        body: JSON.stringify({ title }),
+        body: JSON.stringify({ title, name, description, unit, quantity, price, category }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
-  
+      console.log(response)
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
@@ -28,37 +37,74 @@ const editItemFunc = async (event) => {
   };
 
   const FEeditProfile = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
 
-    const editBtn = document.querySelector("#edit-item");
-    const submitBtn = document.querySelector("#create-edit-btn");
-    const titleInpt = document.querySelector("#item-title");
-    const nameInpt = document.querySelector("#item-name");
-    const descptInpt = document.querySelector("#item-description");
-    const unitInpt = document.querySelector("#item-unit");
-    const quntyInpt = document.querySelector("#item-quantity");
-    const priceInpt = document.querySelector("#item-price");
-    const catInpt = document.querySelector("#item-categories");
-    
-    titleInpt.value = "Something"
-    submitBtn.textContent = "Save Changes"
- 
-    
+
+    if(event.target !== event.currentTarget) {
+      //gets the information for the item from the card
+      const itemTitle = event.currentTarget.querySelector(".title").textContent.trim();
+      const itemName = event.currentTarget.querySelector(".item-name").textContent.trim();
+      const itemDescpt = event.currentTarget.querySelector(".item-description").textContent.trim();
+      const itemUnit = event.currentTarget.querySelector(".item-unit").textContent.trim();
+      const itemQunty = event.currentTarget.querySelector(".item-quantity").textContent.trim();
+      const itemPrice = event.currentTarget.querySelector(".item-price").textContent.trim();
+      const itemCat = event.currentTarget.querySelector(".item-categories").textContent.trim();
+
+      //selects the inputs in the modal for each feild
+      const submitBtn = document.querySelector("#create-edit-btn");
+      const titleInpt = document.querySelector("#item-title");
+      const nameInpt = document.querySelector("#item-name");
+      const descptInpt = document.querySelector("#item-description");
+      const unitInpt = document.querySelector("#item-unit");
+      const quntyInpt = document.querySelector("#item-quantity");
+      const priceInpt = document.querySelector("#item-price");
+      const catInpt = document.querySelector("#item-categories");
+      
+      //inputs the values of the card into the modal for editing
+      titleInpt.value = itemTitle;
+      nameInpt.value = itemName;
+      descptInpt.value = itemDescpt;
+      unitInpt.value = itemUnit;
+      quntyInpt.value = itemQunty;
+      priceInpt.value = itemPrice;
+      catInpt.value = itemCat;
+      submitBtn.textContent = "Save Changes"
+  
+
+    }
 }
 
+//selects all card with item card class, puts them into an array and adds an event listener
+[...document.querySelectorAll('.item-card')].forEach(function(item) {
+  item.addEventListener('click', FEeditProfile);
+})
+
 const openModal = () => {
+
+  //opens the modal with the edit button is clicked
+  clearForm();
+
   const modal = document.getElementById("id03");
   modal.setAttribute("style","display:block; width:auto")
 
   console.log("YOU ARE IN FUNCTION")
 
+  const formSelector = document.querySelector('#create-edit-form');
+  formSelector.setAttribute("class","modal-content animate editItemForm")
+
+  document.querySelector('.editItemForm').addEventListener('submit', editItemFunc);
+
 
 }
 
-document.querySelector('#edit-item').addEventListener('click', FEeditProfile);
+// document.querySelector('.item-card').addEventListener('click', FEeditProfile);
 
-document.querySelector('#edit-item').addEventListener('click', openModal);
+//selects all buttons with edit item class, puts them into an array and adds an event listener
+[...document.querySelectorAll('.edit-item')].forEach(function(item) {
+  item.addEventListener('click', openModal);
+})
 
-  // document
-  // .querySelector('.editButton')
-  // .addEventListener('submit', editItemFunc);
+
+
+
+// document.querySelector('.editItemForm').addEventListener('submit', editItemFunc);
--- public/js/newItem.js
+++ public/js/newItem.js
@@ -1,6 +1,7 @@
 const newItemFunc = async (event) => {
     event.preventDefault();
-  
+    
+
     const title = document.querySelector('#item-title').value.trim();
     const item_name= document.querySelector('#item-name').value.trim();
     const item_description = document.querySelector('#item-description').value.trim();
@@ -29,6 +30,44 @@ const newItemFunc = async (event) => {
     }    
 };
 
-  document
-  .querySelector('.newItemForm')
-  .addEventListener('submit', newItemFunc);
+//opens the modal with the edit button is clicked
+const openCreateModal = () => {
+    clearForm();
+    
+    const modal = document.getElementById("id03");
+    modal.setAttribute("style","display:block; width:auto")
+
+    const formSelector = document.querySelector('#create-edit-form');
+    formSelector.setAttribute("class","modal-content animate newItemForm")
+
+    document
+    .querySelector('.newItemForm')
+    .addEventListener('submit', newItemFunc);
+
+  }
+
+const clearForm = () => {
+    console.log("YOU ARE IN CLEAR FORM")
+    //selects the inputs in the modal for each feild
+    const submitBtn = document.querySelector("#create-edit-btn");
+    const titleInpt = document.querySelector("#item-title");
+    const nameInpt = document.querySelector("#item-name");
+    const descptInpt = document.querySelector("#item-description");
+    const unitInpt = document.querySelector("#item-unit");
+    const quntyInpt = document.querySelector("#item-quantity");
+    const priceInpt = document.querySelector("#item-price");
+    const catInpt = document.querySelector("#item-categories");
+    
+    //clears the values of the card into the modal for creating a new item
+    titleInpt.value = "";
+    nameInpt.value = "";
+    descptInpt.value = "";
+    unitInpt.value = "";
+    quntyInpt.value = "";
+    priceInpt.value = "";
+    catInpt.value = "";
+    submitBtn.textContent = "Post Item"
+}
+
+  document.querySelector('#add-item').addEventListener('click', openCreateModal);
+
