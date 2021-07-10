9cecd33439aaeb1d89110a02807f09e450361aea
aanolan51 <a.a.nolan@hotmail.com>
2021-07-10 13:53:23 -0700

    Updated cart api route

--- controllers/api/cartRoutes.js
+++ controllers/api/cartRoutes.js
@@ -5,21 +5,12 @@ const withAuth = require('../../utils/auth');
 //Get cart
 router.get('/', async (req, res) => {
     try {
-        const userData = await User.findOne({
-            where: {id: req.session.user_id}, 
-            attributes: { exclude: ['password'] },
-          });
-        const userInfo = userData.get({ plain: true });
-    res.render('cart', { 
-        userInfo, 
+    res.render('cart', {
       logged_in: req.session.logged_in 
     });
   } catch (err) {
   res.status(500).json(err);
   }
 });
-
-
-
 
 module.exports = router;
